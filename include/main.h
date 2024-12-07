0e257d369d90eba75a1851a33a272f2aaee832d4
kinyarasam <skinyara.30@gmail.com>
2024-12-07 09:53:37 +0300

    feat: render a window.

--- include/main.h
+++ include/main.h
@@ -1,4 +1,8 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 800
+#define WINDOW_TITLE "RubikVisualizer"
+
 #endif /** __MAIN_H__ */
--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
 #include "main.h"
+#include <GL/glut.h>
+
+/**
+ * display - Handles the rendering of the OpenGL scene
+ */
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // PlaceHolder for Cube rendering
+    glutSwapBuffers();
+}
 
 /**
  * main - Code entry point
  * 
- * Returns: always 0.
+ * Return: always 0.
  *  otherwise 1;
  */
-int main(void) {
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow(WINDOW_TITLE);
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
     return 0;
 }
